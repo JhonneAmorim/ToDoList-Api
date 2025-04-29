18c925658da9327ef311027866d0cf779110c4ec
Jhonne Amorim <106161488+JhonneAmorim@users.noreply.github.com>
2025-04-29 00:49:44 -0300

    feat: Rotas para api todo list

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\TodoListController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,3 +10,9 @@ Route::get('/users/{id}', [UserController::class, 'show']);
 Route::post('/users', [UserController::class, 'store']);
 Route::put('/users/{id}', [UserController::class, 'update']);
 Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+Route::get('/todolist', [TodoListController::class, 'index']);
+Route::get('/todolist/{id}', [TodoListController::class, 'show']);
+Route::post('/todolist', [TodoListController::class, 'store']);
+Route::put('/todolist/{id}', [TodoListController::class, 'update']);
+Route::delete('/todolist/{id}', [TodoListController::class, 'destroy']);
